350e19dd41c447e6221675136853771a9ee2515f
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-06-06 19:46:48 +0300

    documented the patch post dto which is a mapped type

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Patch } from '@nestjs/common';
 import { PostsService } from './services/posts.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreatePostDto } from './dtos/create-post.dto';
+import { PatchPostDto } from './dtos/patch-post.dto';
 
 @Controller('posts')
 @ApiTags('Posts')
@@ -12,9 +13,22 @@ export class PostsController {
   public getPosts(@Param('userId') userId: string) {
     return this.postService.findAll(userId);
   }
+
+  @ApiOperation({
+    summary: 'Creates a new Blog Post',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'You get a 201 resposne if your post is created successfully',
+  })
   @Post()
   public createPost(@Body() createPostDto: CreatePostDto) {
     console.log(createPostDto);
     return this.postService.createPost(createPostDto);
+  }
+
+  @Patch()
+  public updatePost(@Body() patchPostDto: PatchPostDto) {
+    console.log(patchPostDto);
   }
 }
