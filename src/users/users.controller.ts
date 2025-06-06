234d64d750b01730f9f16eefb70a67e89c7b8662
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-06-06 08:26:51 +0300

    Learnt about routing decorators

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,26 @@
-import { Controller, Get, Post, Param, Query, Body, Req } from '@nestjs/common';
-import { Request } from 'express';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Query,
+  Body,
+  Headers,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id/:optional?')
-  public getUsers(@Param() params: any, @Query() query: any) {
-    console.log(query);
-    console.log(params);
-    return `You sent a request to the users endpoint ${params.id}`;
+  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
+    console.log(limit);
+    console.log(id);
+    return `You sent a request to the users endpoint ${id}`;
   }
 
   @Post()
-  public createtUsers(@Req() request: Request) {
-    console.log(request.body);
+  public createtUsers(@Body() body: any, @Headers() headers: any) {
+    console.log(body);
+    console.log(headers);
     return 'You sent a post  request to the users endpoint';
   }
 }
