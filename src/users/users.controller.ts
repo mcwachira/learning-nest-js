0ee79851c05cdde6993abaff977fc5b97df57309
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-06-06 10:19:26 +0300

    Learnt about pipes and how to use them for validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,14 +6,26 @@ import {
   Query,
   Body,
   Headers,
+  ParseIntPipe,
+  DefaultValuePipe,
 } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  @Get('/:id/:optional?')
-  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
+  @Get('/:id/?')
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe)
+    limit?: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe)
+    page?: number,
+  ) {
+    console.log(typeof limit);
     console.log(limit);
+    console.log(typeof id);
     console.log(id);
+    console.log(typeof page);
+    console.log(page);
     return `You sent a request to the users endpoint ${id}`;
   }
 
